41a8d30595dfaed6d4276e711eed38b74519e36f
moinul4960 <moinulhasan.4960@gmail.com>
2022-08-30 23:17:12 +0600

    table and database created

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register-admin',[AuthController::class,'registerUser']);
+Route::post('/create-customer',[CustomerController::class,'createUser']);
